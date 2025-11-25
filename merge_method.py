130caf39bc6924c0a049f562a101d06f60706a1f
Apurv Maheta <159132106+Apurv66@users.noreply.github.com>
2025-11-26 00:12:17 +0530

    Add files via upload

--- merge_method.py
+++ merge_method.py
@@ -1,16 +1,17 @@
 import pandas as pd
 
-df_customer = pd.DataFrame({
-    'id': [1,2,3],
-    'name': ['ramesh', 'suresh', 'kalpesh']
-})
+data1 = {
+    'id': [1,2,3,4,5,6,7],
+    'Department': ['HR', 'Sale', 'Finance', 'HR', 'Sale', 'Sale', 'HR']
+}
+data2 = {
+    'emp_id': [1,2,3,4,5,6,7],
+    'Salary': [25000, 20000, 35000, 30000, 22000, 19000, 25000]
+}
 
-df_orders = pd.DataFrame({
-    'id': [1,2,4],
-    'orderAmount': [250,450,500]
-})
+df1 = pd.DataFrame(data1)
+df2 = pd.DataFrame(data2)
 
-# there are 4 type of join inner, outer, left and right
-df_merged = pd.merge(df_customer, df_orders, on="id", how="inner")
+final = df1.merge(df2, left_on='id', right_on='emp_id')
 
-print(df_merged)
+print(final)

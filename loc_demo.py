c6983cd1faf10af09fc336324d34f3065fbe076b
Apurv Maheta <159132106+Apurv66@users.noreply.github.com>
2025-12-26 12:00:04 +0530

    Add files via upload

---
+++ loc_demo.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    "name": ["jateen", "kishaan", "pravin", "govind", "mukesh", "nirmala"],
+    "age": [28, 37, 38, 29, 35, 38],
+    "salary": [45000, 28000, 50000, 60000, 48000, 55000],
+    "performance_score": [93.5, 89, 94, 83.8, 89, 95.5]
+}
+
+df = pd.DataFrame(data)
+
+df.loc[1, 'salary'] = 30000
+df.loc[0, 'name'] = 'manoj'
+print(df)
